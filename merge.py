000c985a968580588176e383dd4767c2d7b2b2ab
fangfang123gh <58161784+fangfang123gh@users.noreply.github.com>
2024-06-04 19:56:10 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -3,12 +3,17 @@ import copy
 path = ''
 path1 = ''
 save_path = ''
+datas = {}
 with open(path, 'r', encoding='utf-8') as f, open(path1, 'r', encoding='utf-8') as f1, open(save_path, 'w', encoding='utf-8') as fout:
-    for line, line1 in zip(f, f1):
+    for line in f:
         data = json.loads(line)
         fout.write(json.dumps(data, ensure_ascii=False)+'\n')
+        datas[data['sentence']] = data
+    for line1 in f1:
         data1 = json.loads(line1)
-        new_data = copy.deepcopy(data)
+        new_data = copy.deepcopy(datas[data1['gold_text']])
         new_data['sentence'] = data1['text']
-        new_data['srl'] = data1['srl']   
-        fout.write(json.dumps(new_data, ensure_ascii=False)+'\n')     
+        new_data['srl'] = data1['srl'] 
+        
+
+        fout.write(json.dumps(new_data, ensure_ascii=False)+'\n')    
